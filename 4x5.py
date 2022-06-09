7be3442e295412198c74b846a6a677662dbc3ff1
camilotrivino <camilo.trivino@duocuc.cl>
2022-06-09 13:08:45 -0400

    terminado

--- 4x5.py
+++ 4x5.py
@@ -9,12 +9,22 @@ for y in range(4):
     cont_fila=0
     for x in range(5):
         cont_fila+=matriz[y][x]
-    print(cont_fila)
+    print(f"fila {y+1}: {cont_fila}")
 print("----------")
 for x in range(5):
     cont_columna=0
     for y in range(4):
         cont_columna+=matriz[y][x]
-    print(cont_columna)
-
-        
+    print(f"columna {x+1}: {cont_columna}")
+diagonal=0
+for i in range(4):
+    for j in range(5):
+        if i==j:
+            diagonal+=matriz[i][j]
+print(f"la suma diagonal es: {diagonal}")
+impar=0
+for i in range(4):
+    for j in range(5):
+        if matriz[i][j]%2==1:
+            impar+=1
+print(f"cantidad impares: {impar}")
